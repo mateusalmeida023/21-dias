913612f30f1894875a9f6c40a190accbbdbd0e06
Mateus Almeida <mateusalmeida1212@gmail.com>
2025-02-25 16:25:02 -0300

    Exercícios do Dia 10 e Dia 11 concluídos

---
+++ Dia 10-11-12/index.js
@@ -0,0 +1,177 @@
+/*EXERCÍCIO 01
+let arrayUsuario = [];
+let num = parseInt(prompt("Insira o número desejado:"));
+let i;
+
+for(i = 0; i < 10; i++){
+    const numero = parseInt(prompt("Informe um número:"));
+
+    arrayUsuario[i] = numero;
+}
+
+let indices = [];
+let contador = 0;
+for(i = 0; i < 10; i++){
+    if(num === arrayUsuario[i]){
+        indices[contador] = i;
+        contador++;
+    }    
+}
+
+console.log("O número " + num + " aparece nos índices " + indices);
+
+*/
+
+/*EXERCÍCIO 02
+let arrayDeNumeros = [];
+
+
+for(let i = 4; i >= 0; i--){
+    let num = parseInt(prompt('Informe o número:'));
+
+    arrayDeNumeros[i] = num;
+}
+
+console.log(arrayDeNumeros);
+
+*/
+
+/*EXERCÍCIO 03
+let quant = parseInt(prompt("Quantos números irão ser inseridos:"));
+
+let arrayNumeros = []
+let arrayContrario = []
+let j = quant-1;
+
+for(let i = 0; i < quant; i++){
+    let num = parseInt(prompt("Informe o " + (i+1) + "o número:"));
+    
+    arrayNumeros[i] = num;
+    arrayContrario[j] = num;
+    j--;
+}
+
+console.log("Array inserido: " + arrayNumeros);
+console.log("Array contrário: " + arrayContrario);
+
+*/
+
+/*EXERCÍCIO 04
+
+let num = parseInt(prompt("Insira um número:"));
+
+let fibonacci = []
+
+fibonacci[0] = num - 1;
+fibonacci[1] = num;
+
+for(let i = 2; i < 10; i++){
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+
+console.log("Fibonacci: " + fibonacci);
+
+*/
+
+////////////////////////////////////////////////// DIA 11 ///////////////////////////////////////////
+
+/*EXERCÍCIO 01
+
+let arrayAlunos = []
+let arrayNotas = []
+let i = 1;
+let soma = 0;
+
+let continuar = true;
+
+while(continuar){
+    let aluno = prompt("Insira o nome do aluno:")
+    arrayAlunos[i] = aluno;
+
+    let nota = parseFloat(prompt("Insira a nota do aluno " + aluno + ":"))
+    arrayNotas[i] = nota;
+
+    soma += nota;    
+    continuar = prompt("Se você deseja continuar digite 1:");
+    if(continuar != 1){
+        continuar = false;
+        continue;
+    }
+    i++;
+
+}
+
+console.log("A nota dos alunos é: " + arrayNotas);
+
+let media = soma / i;
+
+console.log("A média geral das notas dos " + i + " alunos é: " + media);
+
+*/
+
+/*EXERCÍCIO 02
+
+let modelos = []
+let anos = []
+let valor = []
+
+let continuar = true;
+
+let i = 0;
+
+while(continuar){
+    let m = prompt("Insira o modelo do carro:");
+    modelos[i] = m;
+
+    let a = parseInt(prompt("Insira o ano do carro:"))
+    anos[i] = a;
+
+    let v = parseFloat(prompt("Insira o valor do carro:"));
+    valor[i] = v;
+
+    i++;
+
+    continuar = prompt("Você deseja cadastrar mais um carro: Se sim, se sim digite 1");
+    if( continuar != 1 ){
+        continuar = false;
+        continue;
+    }
+}
+
+for(let k = 0; k < i; k++){
+    console.log(' ' + modelos[k] + ' , ' + anos[k] + ' , R$' + valor[k]);
+}
+
+let ordenaModelo = 0
+let ordenaAno = 0
+let ordenaValor = 0
+
+for(let y = 0; y < i - 1; y++){
+    for(let z = 0; z < i - y - 1; z++){
+        if(valor[z] > valor[z+1]){
+            
+            ordenaModelo = modelos[z]
+            modelos[z] = modelos[z+1]
+            modelos[z+1] = ordenaModelo
+
+            ordenaAno = anos[z]
+            anos[z] = anos[z+1]
+            anos[z+1] = ordenaAno
+
+            ordenaValor = valor[z]
+            valor[z] = valor[z+1]
+            valor[z+1] = ordenaValor
+
+        }
+    }
+}
+
+
+console.log('Carros Ordenados:')
+for(i = 0; i < valor.length; i++){
+    console.log(modelos[i] + ' - ' + anos[i] + ' - R$' + valor[i]);
+}
+
+*/
+
+
